eef85fa4e1e7aa99dfea8a2469cd6b5f60810c43
yusufkashraf <y.kamal26791@gmail.com>
2026-08-21 02:55:25 -0600

    Remove DRIVE_SPEED from Config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Config.java
@@ -2,9 +2,6 @@ package org.firstinspires.ftc.teamcode.robot;
 
 public class Config {
 
-    // Drivetrain
-    public static double DRIVE_SPEED = 1.0;
-
     // Intake
     public static double INTAKE_POWER = 1.0;
     public static double OUTTAKE_POWER = -1.0;
