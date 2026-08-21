8cf4e0292296ee7eb1bea30dd9c214095ce0af5a
yusufkashraf <y.kamal26791@gmail.com>
2026-08-21 01:41:46 -0600

    Create example subsystem for future mechanisms

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ExampleMechanism.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+//state machines are life.
+public class ExampleMechanism {
+    private DcMotorEx example;
+
+    public enum ExampleState {
+        IDLE,
+        INTAKE,
+        OUTTAKE
+    }
+
+    private ExampleState currentState = ExampleState.IDLE;
+
+    public ExampleMechanism(DcMotorEx example) {
+        this.example = example;
+        this.example.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void setState (ExampleState state) {
+        currentState = state;
+    }
+
+    public ExampleState getState() {
+        return currentState;
+    }
+
+    public void update() {
+        switch (currentState) {
+            case IDLE:
+                example.setPower(0);
+                break;
+
+            case INTAKE:
+                example.setPower(1);
+                break;
+
+            case OUTTAKE:
+                example.setPower(-1);
+                break;
+
+        }
+    }
+}
