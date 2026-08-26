b2920c1a85c525cd9ac7db600c3ee672b5cf9656
yusufkashraf <y.kamal26791@gmail.com>
2026-08-26 02:06:43 -0600

    Add simple auton using method from Drivetrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autons/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autons/DriveTest.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode.opmodes.autons;
 
-public class DriveTest {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.robot.Robot;
+
+@Autonomous(name = "Drive Test")
+public class DriveTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        Robot robot = new Robot(hardwareMap);
+
+        waitForStart();
+
+        if (isStopRequested()) {
+            return;
+        }
+
+        // (we should definitely NOT use this for actual autons - we should use pedro - but this is just for testing)
+        // this is an example of how we can incorporate hardware classes into opmodes
+        robot.drivetrain.drive(0, 0.5, 0);
+        sleep(2000);
+        robot.drivetrain.drive(0, 0, 0);
+        sleep(500);
+        robot.drivetrain.drive(0.5, 0, 0);
+        sleep(2000);
+        robot.drivetrain.drive(0, 0, 0);
+    }
 }
